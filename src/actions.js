82ce11ae84136443c0f85521a806b877d61e5556
prr <Mr.gustav009@gmail.com>
2019-11-16 01:41:22 +0300

    Улучшение функционала redux+react

--- src/actions.js
+++
@@ -1,5 +0,0 @@
-export const inc = () => ( {type:'INC'} )
-
-export const dec = () => ( {type:'DEC'} )
-
-export const rnd = (payload) => ( {type:'RND', payload} )
---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SandBox from './sandbox'
+
+const App = () => {
+	return <SandBox />
+}
+
+export default App
